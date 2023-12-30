7ef0d908dc05641ccea0dde7a633594ad01bc9d6
amaym0n <loco.skitls@gmail.com>
2023-12-30 15:24:22 +0300

    Deleted decorator and added _execute method that logging query and query result

--- allure_pg_client/db_client.py
+++ allure_pg_client/db_client.py
@@ -1,30 +1,25 @@
 from __future__ import annotations
 
 from typing import Any
-from typing import Callable
+from typing import LiteralString
 
 import allure
 from psycopg import connect
-
-
-def db_query_listener(func: Callable[..., Any]) -> Callable[..., Any]:
-    def wrapper(*args, **kwargs) -> Any:
-        result = func(*args, **kwargs)
-        with allure.step(title='Query to DataBase: '):
-            allure.attach(kwargs['query'], name='Query to DataBase', attachment_type=allure.attachment_type.TEXT)
-        with allure.step(title='Result of query: '):
-            allure.attach(str(result), name='Query Result', attachment_type=allure.attachment_type.TEXT)
-        return result
-
-    return wrapper
 
 
 class DBClient:
     def __init__(self, connection_string: str) -> None:
         self.connection_string = connection_string
 
-    @db_query_listener
-    def get_list(self, query: str, params: dict[str, Any] | None = None) -> list[Any]:
+    def _execute(self, query: LiteralString, params: dict[str, Any], fetchall: bool) -> Any:
+        with allure.step(title='Query to DataBase: '):
+            allure.attach(query, name='Query to DataBase', attachment_type=allure.attachment_type.TEXT)
+            self.cursor.execute(query=query, params=params)
+            result = self.cursor.fetchall() if fetchall else self.cursor.fetchone()
+            allure.attach(str(result), name='Query Result', attachment_type=allure.attachment_type.TEXT)
+            return result
+
+    def get_list(self, query: LiteralString, params: dict[str, Any] | None = None) -> list[Any]:
         """
             Args:
                 query (str): The SQL query to execute.
@@ -33,8 +28,7 @@ class DBClient:
         """
         return [value[0] for value in self.select_all(query=query, params=params)]
 
-    @db_query_listener
-    def get_dict(self, query: str, params: dict[str, Any] | None = None) -> dict[Any, Any] | None:
+    def get_dict(self, query: LiteralString, params: dict[str, Any] | None = None) -> dict[Any, Any] | None:
         """
             Args:
                 query (str): The SQL query to execute.
@@ -46,20 +40,16 @@ class DBClient:
             return None
         return {value[0]: value[1] for value in self.select_all(query=query, params=params)}
 
-    @db_query_listener
-    def select_all(self, query: str, params: dict[str, Any] | None = None) -> list[tuple]:
+    def select_all(self, query: LiteralString, params: dict[str, Any] | None = None) -> list[tuple]:
         """
             Args:
-                query (str): The SQL query to execute.
+                query (LiteralString): The SQL query to execute.
                 params (dict[str, Any] | None, optional): The parameters to substitute in the query.
                     Defaults to None.
         """
-        self.cursor.execute(query=query, params=params)
-        result = self.cursor.fetchall()
-        return result
+        return self._execute(query=query, params=params, fetchall=True)
 
-    @db_query_listener
-    def get_first_value(self, query: str, params: dict[str, Any] | None = None) -> Any:
+    def get_first_value(self, query: LiteralString, params: dict[str, Any] | None = None) -> Any:
         """
             Args:
                 query (str): The SQL query to execute.
@@ -71,23 +61,19 @@ class DBClient:
             return None
         return result[0]
 
-    @db_query_listener
-    def get_first_row(self, query: str, params: dict[str, Any] | None = None) -> Any:
+    def get_first_row(self, query: LiteralString, params: dict[str, Any] | None = None) -> Any:
         """
             Args:
-                query (str): The SQL query to execute.
+                query (LiteralString): The SQL query to execute.
                 params (dict[str, Any] | None, optional): The parameters to substitute in the query.
                     Defaults to None.
         """
-        self.cursor.execute(query=query, params=params)
-        result = self.cursor.fetchone()
-        return result
+        return self._execute(query=query, params=params, fetchall=False)
 
-    @db_query_listener
-    def execute(self, query: str, params: dict[str, Any] | None = None) -> None:
+    def execute(self, query: LiteralString, params: dict[str, Any] | None = None) -> None:
         """
             Args:
-                query (str): The SQL query to execute.
+                query (LiteralString): The SQL query to execute.
                 params (dict[str, Any] | None, optional): The parameters to substitute in the query.
                     Defaults to None.
         """
